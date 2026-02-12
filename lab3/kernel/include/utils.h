2ae922184648d4f9d7c6378d61746a4604839941
nodyFN <nodyfang123456@gmail.com>
2026-02-12 17:02:55 +0000

    lab3 init

---
+++ lab3/kernel/include/utils.h
@@ -0,0 +1,22 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+static inline uint32_t toLittleEndian(uint32_t val) {
+    return ((val & 0x000000FFU) << 24) |
+           ((val & 0x0000FF00U) << 8)  |
+           ((val & 0x00FF0000U) >> 8)  |
+           ((val & 0xFF000000U) >> 24);
+}
+
+static inline uint32_t toBigEndian(uint32_t val) {
+    return toLittleEndian(val);
+}
+
+struct KernelInfo{
+    uint64_t hartid;
+    void* dtb_addr;
+    uint64_t initrd_start_addr;
+    uint64_t initrd_end_addr;
+};
+
+#endif
